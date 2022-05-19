9b9d73505b3f6ef969df5e3a1e4a5b7152f24943
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-05-19 12:30:47 +0200

    Add basic structur functions in script.js

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
-let gameData = ["scissors", " paper", "rock", "lizard", "spock" ];
+let dataGame = ["rock", "paper", "scissor", "lizard", "spock"]
+let playerInput;
+let computerChoice;
+
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorButton =  document.getElementById("scissor");
+const lizardButton = document.getElementById("lizard");
+const spockButton = document.getElementById("spock");
 
 
-
-
-
-document.getElementById("scissors").addEventListener("click", function () {
-/*let scissors = gameData[0]
-    console.log(scissors)*/
-computerSelection()
- })
-
-function computerSelection(){
-    let random = Math.floor(Math.random() * gameData.length)
-    return gameData[random]
+function getRandomItem() {
+    computerChoice = dataGame[Math.floor(Math.random() * dataGame.length)];
 }
 
 
+rockButton.addEventListener("click", function () {
+
+    playerInput = rockButton.id;
+    console.log(playerInput)
+    getRandomItem();
+    game();
+})
+
+paperButton.addEventListener("click", function () {
+    playerInput = paperButton.id;
+    console.log(playerInput)
+    // getRandomItem();
+    // game()
+})
+
+scissorButton.addEventListener("click", function () {
+    playerInput = scissorButton.id;
+    console.log(playerInput)
+    // getRandomItem()
+    // game()
+})
+
+lizardButton.addEventListener("click", function () {
+   playerInput = lizardButton.id;
+    console.log(playerInput)
+    // getRandomItem()
+    // game()
+})
+
+spockButton.addEventListener("click", function () {
+playerInput = spockButton.id;
+    console.log(playerInput)
+    // getRandomItem()
+    // game()
+})
 
 
 
-// document.getElementById("scissors").addEventListener("click", function () {
-//   const randomPlayer = Math.floor(Math.random() * gameData.length)
-//     console.log( randomPlayer, gameData[randomPlayer]);
-// })
+function game() {
+    if(playerInput === "rock" && computerChoice === "rock") {
+        console.log("draw")
+    }
+}
