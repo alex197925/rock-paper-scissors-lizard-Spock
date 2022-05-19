49f652c2ffdf19813ba524cacfe2ccf83045208c
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-05-19 16:59:39 +0200

    Add style

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ const scissorButton =  document.getElementById("scissor");
 const lizardButton = document.getElementById("lizard");
 const spockButton = document.getElementById("spock");
 
+let userScore = 0;
+let computerScore = 0;
+
+const updatePlayer = document.querySelector('.playerUpdateScore')
+const updateBot = document.querySelector('.botUpdateScore');
+
+
+
+
+const playerResult = document.getElementById("player-score");
+const tieScore = document.getElementById("tie-score");
 
 function getRandomItem() {
     computerChoice = dataGame[Math.floor(Math.random() * dataGame.length)];
@@ -15,7 +26,6 @@ function getRandomItem() {
 
 
 rockButton.addEventListener("click", function () {
-
     playerInput = rockButton.id;
     console.log(playerInput)
     getRandomItem();
@@ -25,35 +35,101 @@ rockButton.addEventListener("click", function () {
 paperButton.addEventListener("click", function () {
     playerInput = paperButton.id;
     console.log(playerInput)
-    // getRandomItem();
-    // game()
+    getRandomItem();
+    game()
 })
 
 scissorButton.addEventListener("click", function () {
     playerInput = scissorButton.id;
     console.log(playerInput)
-    // getRandomItem()
-    // game()
+    getRandomItem()
+    game()
 })
 
 lizardButton.addEventListener("click", function () {
    playerInput = lizardButton.id;
     console.log(playerInput)
-    // getRandomItem()
-    // game()
+    getRandomItem()
+    game()
 })
 
 spockButton.addEventListener("click", function () {
 playerInput = spockButton.id;
     console.log(playerInput)
-    // getRandomItem()
-    // game()
+    getRandomItem()
+    game()
 })
 
 
 
 function game() {
-    if(playerInput === "rock" && computerChoice === "rock") {
-        console.log("draw")
+
+    // Tie
+    if (playerInput === computerChoice) {
+        tieScore.innerText = "To tie!"
+        userScore++;
+        updatePlayer.innerText = userScore;
+        computerScore++;
+        updateBot.innerText = computerScore;
     }
+    //Rock
+    else if (playerInput === "rock") {
+        if (computerChoice === "paper" || computerChoice === "spock") {
+            updateBot.innerText = computerScore;
+            computerScore++;
+        } else {
+            playerResult.innerText = "winner";
+            userScore++;
+            playerResult.innerText = userScore;
+        }
+    }
+    // Scissor
+    else if (playerInput === "scissor") {
+        if (computerChoice === "rock" || computerChoice === "spock") {
+            updateBot.innerText = computerScore;
+            computerScore++;
+        }else {
+            playerResult.innerText = "winner";
+            userScore++;
+            playerResult.innerText = userScore;
+        }
+    }
+    //  Lizard
+    else if (playerInput === "lizard") {
+        if (computerChoice === "rock" || computerChoice === "scissor"){
+            updateBot.innerText = computerScore;
+            computerScore++;
+        }else {
+            playerResult.innerText = "winner";
+            userScore++;
+            playerResult.innerText = userScore;
+        }
+    }
+   //    Paper
+    else if (playerInput === "paper") {
+        if (computerChoice === "scissor" || computerChoice === "lizard") {
+            updateBot.innerText = computerScore;
+            computerScore++;
+        }else {
+            playerResult.innerText = "winner";
+            userScore++;
+            playerResult.innerText = userScore;
+        }
+    }
+   //   Spock
+    else if (playerInput === "spock") {
+        if (computerChoice === "lizard" || computerChoice === "paper") {
+            updateBot.innerText = computerScore;
+            computerScore++;
+        }else {
+            playerResult.innerText = "winner";
+            userScore++;
+            playerResult.innerText = userScore;
+        }
+    }
+
+
+
 }
+
+
