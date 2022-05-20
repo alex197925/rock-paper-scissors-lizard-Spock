8f99674850fd56428908023cfe61230d950476ac
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-05-20 14:49:20 +0200

    Add basic of html, main function and geted all buttons in js file

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-# rock-paper-scissors-lizard-Spock
+## Introduction:
+Rock, paper, and scissors game is a simple fun game in which both the players have to make a rock, paper, or scissors. It has only two possible outcomes a draw, or a win for one player and a loss for the other player. We will be designing the game using JavaScript where a player will be playing against the computer. In total there will be 10 moves. The player has to choose one option among the rock, paper, and scissors. A random option will be generated from the computer’s side and the one who wins will get one point every time. After 10 moves are over the final result will be displayed on the screen with a button to restart the game. The game will be completely responsive so that it can be played on every device.
 
 
-# Learning objectives
-- Generate random outcomes
-- If statements
-- Coming up with a basic algoritm from scratch
+## The HTML Layout:
+- [ ]  Create basic html layout
 
-## Features
 
-- [ ] Have 5 buttons
-- [ ]  Have 5 buttons that the player can press to change their pick
-- [ ] Let the player know they won (no alert box)
-- [ ] Have a reset button
-
-# Progress Guide
-
-- [ ] Store game data
+## Javascript Part
+- [ ] Create a function game() that will contain all the logic of the game.
+- [ ] Create a function playGame() and inside the function use DOM manipulation to get hold of all the three buttons we created in HTML for player input. Create an array playerOptions which will contain all three buttons as its elements. Similarly, create an array for computer options.
+- [ ] Use forEach() loop on playerOptions so that we can add an event listener on all buttons with a single piece of code. Inside the loop increment moves counter by 1 display moves left on the screen by subtracting moves from 10. Generate a random value for the computer option and compare it with the player’s input.
+- [ ] Create a function winner() which will receive two arguments one the player’s input and the other the computer’s option  The function will decide who wins the point among the player and computer.
+- [ ] Create a function gameOver() which will display the final result with reload button. The function will be called when moves will become equals to 10.
+- [ ] Call the playGame() function inside the game() function.
+- [ ] Now call the game() function at the end of the file.
--- script.js
+++ script.js
@@ -1,135 +1,80 @@
-let dataGame = ["rock", "paper", "scissor", "lizard", "spock"]
-let playerInput;
-let computerChoice;
-
-const rockButton = document.getElementById("rock");
-const paperButton = document.getElementById("paper");
-const scissorButton =  document.getElementById("scissor");
-const lizardButton = document.getElementById("lizard");
-const spockButton = document.getElementById("spock");
-
-let userScore = 0;
-let computerScore = 0;
-
-const updatePlayer = document.querySelector('.playerUpdateScore')
-const updateBot = document.querySelector('.botUpdateScore');
-
-
-
-
-const playerResult = document.getElementById("player-score");
-const tieScore = document.getElementById("tie-score");
-
-function getRandomItem() {
-    computerChoice = dataGame[Math.floor(Math.random() * dataGame.length)];
-}
-
-
-rockButton.addEventListener("click", function () {
-    playerInput = rockButton.id;
-    console.log(playerInput)
-    getRandomItem();
-    game();
-})
-
-paperButton.addEventListener("click", function () {
-    playerInput = paperButton.id;
-    console.log(playerInput)
-    getRandomItem();
-    game()
-})
-
-scissorButton.addEventListener("click", function () {
-    playerInput = scissorButton.id;
-    console.log(playerInput)
-    getRandomItem()
-    game()
-})
-
-lizardButton.addEventListener("click", function () {
-   playerInput = lizardButton.id;
-    console.log(playerInput)
-    getRandomItem()
-    game()
-})
-
-spockButton.addEventListener("click", function () {
-playerInput = spockButton.id;
-    console.log(playerInput)
-    getRandomItem()
-    game()
-})
-
-
-
+// Main function
 function game() {
+    let playerScore;
+    let computerScore;
+    let moves = 0;
 
-    // Tie
-    if (playerInput === computerChoice) {
-        tieScore.innerText = "To tie!"
-        userScore++;
-        updatePlayer.innerText = userScore;
-        computerScore++;
-        updateBot.innerText = computerScore;
+
+    function playGame() {
+        const rockBtn = document.getElementById("rock");
+        const paperBtn = document.getElementById("paper");
+        const scissorsBtn = document.getElementById("scissors");
+        const lizardBtn = document.getElementById("lizard");
+        const spockBtn = document.getElementById("spock");
     }
-    //Rock
-    else if (playerInput === "rock") {
-        if (computerChoice === "paper" || computerChoice === "spock") {
-            updateBot.innerText = computerScore;
-            computerScore++;
-        } else {
-            playerResult.innerText = "winner";
-            userScore++;
-            playerResult.innerText = userScore;
-        }
-    }
-    // Scissor
-    else if (playerInput === "scissor") {
-        if (computerChoice === "rock" || computerChoice === "spock") {
-            updateBot.innerText = computerScore;
-            computerScore++;
-        }else {
-            playerResult.innerText = "winner";
-            userScore++;
-            playerResult.innerText = userScore;
-        }
-    }
-    //  Lizard
-    else if (playerInput === "lizard") {
-        if (computerChoice === "rock" || computerChoice === "scissor"){
-            updateBot.innerText = computerScore;
-            computerScore++;
-        }else {
-            playerResult.innerText = "winner";
-            userScore++;
-            playerResult.innerText = userScore;
-        }
-    }
-   //    Paper
-    else if (playerInput === "paper") {
-        if (computerChoice === "scissor" || computerChoice === "lizard") {
-            updateBot.innerText = computerScore;
-            computerScore++;
-        }else {
-            playerResult.innerText = "winner";
-            userScore++;
-            playerResult.innerText = userScore;
-        }
-    }
-   //   Spock
-    else if (playerInput === "spock") {
-        if (computerChoice === "lizard" || computerChoice === "paper") {
-            updateBot.innerText = computerScore;
-            computerScore++;
-        }else {
-            playerResult.innerText = "winner";
-            userScore++;
-            playerResult.innerText = userScore;
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
